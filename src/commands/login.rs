cf96be22623ca775d376f5648c608636ca03bb1a
knight42 <anonymousknight96@gmail.com>
2018-02-19 01:49:24 +0800

    style: rustfmt the code

--- src/commands/login.rs
+++ src/commands/login.rs
@@ -1,14 +1,14 @@
 use clap::{App, ArgMatches, SubCommand};
-use rpassword::prompt_password_stdout;
 use context::Context;
-use std::io::{self, Write};
+use rpassword::prompt_password_stdout;
 use std::fs;
+use std::io::{self, Write};
 
 pub(crate) struct Login;
 
 #[derive(Deserialize)]
 struct Token {
-    token: String
+    token: String,
 }
 
 impl super::Commander for Login {
@@ -24,10 +24,15 @@ impl super::Commander for Login {
         io::stdin().read_line(&mut username)?;
         let username = username.trim();
         let password = prompt_password_stdout("Password: ")?;
-        let mut r = ctx.post(remote).basic_auth(username, Some(password)).send()?;
+        let mut r = ctx.post(remote)
+            .basic_auth(username, Some(password))
+            .send()?;
         exit_on_error!(r);
         let t: Token = r.json()?;
-        let mut f = fs::OpenOptions::new().write(true).create(true).open(ctx.homedir.join("token"))?;
+        let mut f = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(ctx.homedir.join("token"))?;
         f.write_all(t.token.as_bytes())?;
         f.sync_data()?;
         println!("Login successfully.");
--- src/commands/logout.rs
+++ src/commands/logout.rs
@@ -9,7 +9,10 @@ impl super::Commander for Logout {
         SubCommand::with_name("logout").about("Log out")
     }
     fn exec(ctx: &Context, _: &ArgMatches) -> ::Result<()> {
-        let f = fs::OpenOptions::new().write(true).create(true).open(ctx.homedir.join("token"))?;
+        let f = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(ctx.homedir.join("token"))?;
         f.set_len(0)?;
         println!("Logout successfully.");
         Ok(())
