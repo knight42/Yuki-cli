67b7bb58a8255eca6f03eecf812680c97df1ff2a
knight42 <anonymousknight96@gmail.com>
2018-02-24 02:58:50 +0800

    chore: update travis config

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -15,9 +15,8 @@ docker run \
         && sudo chown -R rust:rust target/ /home/rust/.cargo/registry/ \
         && rustup update stable \
         && cargo build --target=$TARGET --release \
-        && sudo chown -R $LOCAL_USER target/$TARGET/release/"
+        && sudo chown $LOCAL_USER target/$TARGET/release/$BIN_NAME"
 
 cd "$TARGET_DIR" || exit 1
 
-mv yuki_cli yuki
-tar -czf "$CUR_DIR/$BIN_NAME-$TRAVIS_TAG.$TARGET.tar.gz" yuki
+tar -czf "$CUR_DIR/$BIN_NAME-$TRAVIS_TAG.$TARGET.tar.gz" "$BIN_NAME"
